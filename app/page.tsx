d629896eb86a28152a4d252f124780db40dd28e1
CotejaDoido <joaovictor.poncelima1@gmail.com>
2024-11-21 20:32:45 -0300

    feat: our barbers component implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "@/components/about";
 import { HomeHero } from "@/components/home-hero";
 import { Navbar } from "@/components/navbar";
+import { OurBarbers } from "@/components/our-barbers";
 import { OurServices } from "@/components/our-services";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <HomeHero />
       <About />
       <OurServices />
+      <OurBarbers />
     </>
   );
 }
